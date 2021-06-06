052c1f1210cf42e803ed96fce8b80afacfce34fd
edwinmwiti <emwiti658@gmail.com>
2021-06-06 18:44:50 +0300

    just adc

---
+++ adc/src/main.cpp
@@ -0,0 +1,23 @@
+// #include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void){
+  // inits
+  DDRB |= (1 << PB0);
+
+  // event loop
+  while (1){
+    PORTB = 0b00000010; // turn on the first led pin on portB
+    _delay_ms(500);
+
+    PORTB = 0b00000000; // turn off all leds
+    _delay_ms(500);
+
+
+  }
+
+  return 0;
+  
+
+}
