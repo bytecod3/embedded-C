f64b9b23f1dd1a636e2744a63a08a22f00e26e9a
edwinmwiti <emwiti658@gmail.com>
2021-06-06 19:15:29 +0300

    watch out! blinkenlights

---
+++ persistence_of_vision/src/main.cpp
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+void pov_display(uint8_t one_byte){
+  // a function to animate the LEDs
+  PORTB = one_byte;
+
+  _delay_ms(2);
+
+}
+
+int main(){
+
+  DDRB = 0xFF; // set all the pins on DDRB to output
+
+  while (1){ // event loop
+
+    pov_display(0b00001001); // just some random combos!
+    pov_display(0b11110000);
+    pov_display(0b10101010);
+    pov_display(0b01010101);
+    pov_display(0b00001001);
+    pov_display(0b11001100);
+    pov_display(0b11100011);
+    pov_display(0b00110011);
+
+    PORTB = 0;
+    _delay_ms(10); 
+    
+  }
+  
+  return 0;
+}
