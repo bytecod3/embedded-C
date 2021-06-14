77f98154fdacd4e2dd83c01a6fbfd94c02e17e59
edwinmwiti <emwiti658@gmail.com>
2021-06-14 17:12:14 +0300

    finished bitwise operations

--- bitwise_operations/src/main.c
+++ bitwise_operations/src/main.c
@@ -8,9 +8,23 @@
 int main(){
 	uint8_t PORTB_VAL = 0b11000011;
 	
-	PORTB_VAL |= BIT2_MASK;
-
+	// setting bit2. Alt: PORTB_VAL |= (1<<2) .OR
+	PORTB_VAL |= BIT2_MASK; 
 	PRINT_BIN(PORTB_VAL);
+	
+	// toggle bit 2 keeping others same. XOR
+	PORTB_VAL ^= (1<<2); 
+	PRINT_BIN(PORTB_VAL);
+	
+	// toggle more than one bit. XOR
+	PORTB_VAL ^= (1<<2) | (1<<4); // bit 2 and 4
+	
+	// clearing bits with AND
+	PORTB_VAL &= ~(1<<2);	// clear bit 2
+	
+	// clearing more than two bits
+	PORTB_VAL &= ~((1<<2)| (1<<3)); // clear bit 2 and 4
+		
 	
 	return 0;
 } 
